7b0accd847398d23eeb516d37cab25e82b372f7a
Nick Steele <you@example.com>
2020-04-28 15:47:14 +0930

    Added image of banana to this branch

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CONGRATULATIONS!!!!');
+  res.send('<html><body><h1>Congratulations! *Node.js Dev Environment 2 inside Docker!*</h1><img src="https://www.jquery-az.com/html/images/banana.jpg" alt="Banana" width="300" height="300"></body></html>');
 });
 
 app.listen(PORT, HOST);
