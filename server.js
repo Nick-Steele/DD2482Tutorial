f0d1049815b48776b8003b2c76226932fc586046
Nick Steele <you@example.com>
2020-04-28 15:31:15 +0930

    Added image and updated file

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CONGRATULATIONS!!!!');
+  res.send('<html><body><h1>Congratulations! *Node.js webb app straight from a Docker container*</h1><img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" alt="Flamingo" width="300" height="300"> </body></html>');
+
 });
 
 app.listen(PORT, HOST);
